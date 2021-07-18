788e02cec41a8c680caba9f75c1cabf5e14a45de
Michel <michel_schwarz@ymail.com>
2021-07-19 00:37:29 +0200

    fix: add button and display

--- Get_values/Get_values.ino
+++ Get_values/Get_values.ino
@@ -1,34 +1,66 @@
 #include <ArduinoJson.h>
 #include "ESP8266HTTPClient.h"
 #include "ESP8266WiFi.h"
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
 #include <Wire.h>
 
-const char* ssid     = "XXXXXXXXXXXXXXXXXXXXX";
-const char* password = "XXXXXXXXXXXXXXXXXXXXX";
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define OLED_RESET     16 
+#define SCREEN_ADDRESS 0x3D ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+#define LOGO_HEIGHT   16
+#define LOGO_WIDTH    16
+
+unsigned long timePressLimit;
+
+const char* ssid     = "xxxxxxx";
+const char* password = "xxxxxxx";
 
 // the ThinkSpeak key and channel to read your own chanel
-const char* apiKeyReadOwn = "XXXXXXXXXXXXXXXXXXXXX";
-const char* apiChannelFeedOwn = "XXXXXXXXXXXXXXXXXXXXX";
+const char* apiKeyReadOwn = "xxxxxxx";
+const char* apiChannelFeedOwn = "xxxxxxx";
 
 // the ThinkSpeak key and channel to read from the second member
-const char* apiKeyReadSecond = "XXXXXXXXXXXXXXXXXXXXX";
-const char* apiChannelFeedSecond = "XXXXXXXXXXXXXXXXXXXXX";
+const char* apiKeyReadSecond = "xxxxxxx";
+const char* apiChannelFeedSecond = "xxxxxxx";
 
 // the ThinkSpeak key and channel to read from the third member
-const char* apiKeyReadThird = "XXXXXXXXXXXXXXXXXXXXX";
-const char* apiChannelFeedThird = "XXXXXXXXXXXXXXXXXXXXX";
+const char* apiKeyReadThird = "xxxxxxx";
+const char* apiChannelFeedThird = "xxxxxxx";
+
+// the key to send data to ThingSpeak
+const char* apiKeyWrite = "xxxxxxx";
 
 // just for one LED, the others depend on the hardware connection
-int red_light_pin= 15;
-int green_light_pin = 14;
-int blue_light_pin = 13;
+int red_light_pin_1 = 1 ;
+int green_light_pin_1 = 3;
+int blue_light_pin_1 = 10;
+
+int red_light_pin_2 = 13;
+int green_light_pin_2 = 12;
+int blue_light_pin_2 = 15;
+
+int red_light_pin_3 = 2;
+int green_light_pin_3 = 0;
+int blue_light_pin_3 = 14;
 
 HTTPClient http;
 WiFiClientSecure client;
 
-const char* pos;
+const char* pos_user_1; 
+const char* pos_user_2; 
+const char* pos_user_3; 
+
+const int sensor_pin = 9; 
+
+int touch_lastState = LOW;  
+int touch_currentState;
  
 void setup() {
  
@@ -42,51 +74,163 @@ void setup() {
   
   client.setInsecure();
 
-  Serial.println("");
-  delay(100);
+  Serial.println("WiFi is connected...");
+
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
       
-  pinMode(red_light_pin, OUTPUT);
-  pinMode(green_light_pin, OUTPUT);
-  pinMode(blue_light_pin, OUTPUT);
+  pinMode(red_light_pin_1, OUTPUT);
+  pinMode(green_light_pin_1, OUTPUT);
+  pinMode(blue_light_pin_1, OUTPUT);
+
+  pinMode(red_light_pin_2, OUTPUT);
+  pinMode(green_light_pin_2, OUTPUT);
+  pinMode(blue_light_pin_2, OUTPUT);
+
+  pinMode(red_light_pin_3, OUTPUT);
+  pinMode(green_light_pin_3, OUTPUT);
+  pinMode(blue_light_pin_3, OUTPUT);
+
+  // activate touch sensor
+  pinMode(sensor_pin, INPUT);
+
+  Serial.println("Pin Mode is done...");
+
+  setColorOwn(0,0,0);
+  setColorUserTwo(0,0,0);
+  setColorUserThree(0,0,0);
+  display.display();
 }
  
 void loop() {
-  char urlRead[100];
+  Serial.println("Loop is starting...");
   
   // create an url string which is sent to thinkspeak
+  char urlRead[100];
   sprintf(urlRead, "https://api.thingspeak.com/channels/%s/feeds.json?api_key=%s&results=1", apiChannelFeedOwn, apiKeyReadOwn);
   readRGBValues(urlRead);
-
-  delay(1000);
+  touchSensor();
 }
 
-void decodeJson(String response){ 
+void decodeJson(String response){
+  
   DynamicJsonDocument doc(1024);
   deserializeJson(doc, response);
-    
-  pos = doc["feeds"][0]["field4"];
-  Serial.println("pos");
-  Serial.println(pos);
-  int posInt = atoi(pos);
 
-  // for example: 0 === arms are above
-  if(posInt == 0){
-    setColor(255, 255, 255);
+  Serial.print("Read from JSON Doc...\n");
+  if(doc["feeds"][0]["field1"]){
+    pos_user_1 = doc["feeds"][0]["field1"]; 
+    Serial.println(pos_user_1);
   }
-  if(posInt == 1){
-    setColor(255, 0, 0);
+  else{
+    Serial.println(pos_user_1);
   }
-  if(posInt == 2){
-    setColor(0, 255, 0);
+
+  if(doc["feeds"][0]["field2"]){
+    pos_user_2 = doc["feeds"][0]["field2"]; 
+    Serial.println(pos_user_2);
   }
+  else{
+    Serial.println(pos_user_2);
+  }
+
+  if(doc["feeds"][0]["field3"]){
+    pos_user_3 = doc["feeds"][0]["field3"]; 
+    Serial.println(pos_user_3);
+  }
+  else{
+    Serial.println(pos_user_3);
+  }
+
+  int posInt_1 = atoi(pos_user_1);
+  int posInt_2 = atoi(pos_user_2);
+  int posInt_3 = atoi(pos_user_3);
+  
+  Serial.println("pos 1: ");
+  Serial.println(posInt_1);
   Serial.println("\n");
-  Serial.println("-------------------");  
+
+  Serial.println("pos 2: ");
+  Serial.println(posInt_2);
+  Serial.println("\n");
+
+  Serial.println("pos 3: ");
+  Serial.println(posInt_3);
+  Serial.println("\n");
+
+  delay(500);
+  setColorOwn(0, 0, 0);
+  switchUserColorsByPosition(posInt_1, 1);
+  delay(500);
+  setColorUserTwo(0,0,0);
+  switchUserColorsByPosition(posInt_2, 2);
+  delay(500);
+  setColorUserThree(0,0,0);
+  switchUserColorsByPosition(posInt_3, 3);
+  
+}
+
+void switchUserColorsByPosition(int pos, int user){
+
+  if(user == 1){
+    switch (pos) {
+    case 0:
+      setColorOwn(100, 100, 75);
+      break;
+    case 1:
+      setColorOwn(0, 255, 0);
+      break;
+    case 2:
+      setColorOwn(255, 75, 0);
+      break;
+    case 3:
+      setColorOwn(0, 200, 200);
+      break;
+    }
+  }
+
+  if(user == 2){
+    switch (pos) {
+    case 0:
+      setColorUserTwo(100, 100, 75);
+      break;
+    case 1:
+      setColorUserTwo(0, 255, 0);
+      break;
+    case 2:
+      setColorUserTwo(255, 75, 0);
+      break;
+    case 3:
+      setColorUserTwo(0, 200, 200);
+      break;
+    }
+  }
+
+  if(user == 3){
+    switch (pos) {
+    case 0:
+      setColorUserThree(100, 100, 75);
+      break;
+    case 1:
+      setColorUserThree(0, 255, 0);
+      break;
+    case 2:
+      setColorUserThree(255, 75, 0);
+      break;
+    case 3:
+      setColorUserThree(0, 200, 200);
+      break;
+    }
+  }
 }
 
 void readRGBValues(String url){
 
  if(WiFi.status()== WL_CONNECTED){
-  
+
+   Serial.println("Try to get data from ThinkSpeak...");
    http.begin(client, url);
    http.addHeader("Content-Type", "application/json");            
  
@@ -99,6 +243,7 @@ void readRGBValues(String url){
      char responseText[50];
      sprintf(responseText, "Response Code: %d\n", httpResponseCode);
      Serial.print(responseText);
+
      
      decodeJson(response);
  
@@ -113,9 +258,71 @@ void readRGBValues(String url){
  }
 }
 
-void setColor(int red_light_value, int green_light_value, int blue_light_value)
- {
-  analogWrite(red_light_pin, red_light_value);
-  analogWrite(green_light_pin, green_light_value);
-  analogWrite(blue_light_pin, blue_light_value);
+void setColorOwn(int red_light_value, int green_light_value, int blue_light_value)
+{
+  analogWrite(red_light_pin_1, red_light_value);
+  analogWrite(green_light_pin_1, green_light_value);
+  analogWrite(blue_light_pin_1, blue_light_value);
+}
+
+void setColorUserTwo(int red_light_value, int green_light_value, int blue_light_value)
+{
+  analogWrite(red_light_pin_2, red_light_value);
+  analogWrite(green_light_pin_2, green_light_value);
+  analogWrite(blue_light_pin_2, blue_light_value);
+}
+
+void setColorUserThree(int red_light_value, int green_light_value, int blue_light_value)
+{
+  analogWrite(red_light_pin_3, red_light_value);
+  analogWrite(green_light_pin_3, green_light_value);
+  analogWrite(blue_light_pin_3, blue_light_value);
+}
+
+void touchSensor(){
+  touch_currentState = digitalRead(sensor_pin);
+
+  if(touch_lastState == LOW && touch_currentState == HIGH){
+    char urlWrite[100];
+    sprintf(urlWrite, "https://api.thingspeak.com/update.json?api_key=%s&field4=%d", apiKeyWrite, 1);
+    sendTextValue(urlWrite);
+
+    timePressLimit = millis() + 30000;    
+  }
+
+  if(timePressLimit > 0 && millis() < timePressLimit){
+    char urlWrite[100];
+    sprintf(urlWrite, "https://api.thingspeak.com/update.json?api_key=%s&field4=%d", apiKeyWrite, 0);
+    sendTextValue(urlWrite);
+  }
+
+  touch_lastState = touch_currentState;
+}
+
+void sendTextValue(String url){
+
+ if(WiFi.status()== WL_CONNECTED){
+  
+   http.begin(client, url);
+   http.addHeader("Content-Type", "application/json");            
+ 
+   int httpResponseCode = http.GET();   
+
+   if(httpResponseCode > 0){
+     Serial.println("Write data to ThinkSpeak...");
+     String response = http.getString();   
+
+     char responseText[50];
+     sprintf(responseText, "Response Code: %d\n", httpResponseCode);
+     Serial.print(responseText);
+ 
+   }else{
+      Serial.print("Error on sending PUT Request: ");
+      Serial.println(httpResponseCode);
+   }
+   http.end();
+ 
+ }else{
+    Serial.println("Error in WiFi connection");
+ }
 }
--- Send_Values/Send_Values.ino
+++ Send_Values/Send_Values.ino
@@ -5,42 +5,61 @@
 #include <Adafruit_Sensor.h>
 #include <Wire.h>
 
-const char* ssid     = "XXXXXXXXXXXXXXXXXXXXX";
-const char* password = "XXXXXXXXXXXXXXXXXXXXX";
+const char* ssid     = "xxxxxxx";
+const char* password = "xxxxxxx";
 
 // the ThinksSpeak key and channel to write to your own channel
-const char* apiKeyWrite = "XXXXXXXXXXXXXXXXXXXXX";
+const char* apiKeyWrite = "xxxxxxx";
 
-Adafruit_MPU6050 mpu;
+// AD0 to ground
+Adafruit_MPU6050 mpu_right;
+
+// AD0 to vcc
+Adafruit_MPU6050 mpu_left;
+
 
 HTTPClient http;
 WiFiClientSecure client;
 
 void setup() {
- 
+  Wire.begin();
+  
   Serial.begin(115200);
   WiFi.begin(ssid, password); 
  
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
-    Serial.println("Connecting to WiFi..");
+    Serial.println("Connecting to WiFi...");
   }
   
   client.setInsecure();
 
-  if (!mpu.begin()) {
-    Serial.println("Failed to find MPU6050 chip");  
+  // Try to initialize first mpu!
+  if (!mpu_right.begin(0x68)) {
+    Serial.println("Failed to find MPU6050 chip at address 0x68");
     while (1) {
       delay(10);
     }
   }
-  Serial.println("MPU6050 Found!");
-  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
-  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
-  mpu.setFilterBandwidth(MPU6050_BAND_5_HZ);
+  Serial.println("MPU6050 at address 0x68 found!");
 
-  Serial.println("");
-  delay(100);
+  mpu_right.setAccelerometerRange(MPU6050_RANGE_8_G);
+  mpu_right.setGyroRange(MPU6050_RANGE_500_DEG);
+  mpu_right.setFilterBandwidth(MPU6050_BAND_5_HZ);
+  
+
+  // Try to initialize first mpu!
+  if (!mpu_left.begin(0x69)) {
+    Serial.println("Failed to find MPU6050 chip at address 0x69");
+    while (1) {
+      delay(10);
+    }
+  }
+  Serial.println("MPU6050 at address 0x69 found!");
+
+  mpu_left.setAccelerometerRange(MPU6050_RANGE_8_G);
+  mpu_left.setGyroRange(MPU6050_RANGE_500_DEG);
+  mpu_left.setFilterBandwidth(MPU6050_BAND_5_HZ);
 }
  
 void loop() {
@@ -48,7 +67,7 @@ void loop() {
   char urlRead[100];
 
   // getArmPosition() returns the current arm position based on the gyro values
-  sprintf(urlWrite, "https://api.thingspeak.com/update.json?api_key=%s&field4=%d", apiKeyWrite, getArmPosition());
+  sprintf(urlWrite, "https://api.thingspeak.com/update.json?api_key=%s&field1=%d", apiKeyWrite, getEmotion());
   writeRGBValues(urlWrite);
 
   delay(1000);
@@ -70,6 +89,9 @@ void writeRGBValues(String url){
      char responseText[50];
      sprintf(responseText, "Response Code: %d\n", httpResponseCode);
      Serial.print(responseText);
+
+     // if code is used to get Data uncomment this
+     //decodeJson(response);
  
    }else{
       Serial.print("Error on sending PUT Request: ");
@@ -82,33 +104,131 @@ void writeRGBValues(String url){
  }
 }
 
-int getArmPosition(){
-  sensors_event_t a, g, temp;
-  mpu.getEvent(&a, &g, &temp);
+int getEmotion() {
+  String rightPos = getMPUAccelerationRight();
+  String leftPos = getMPUAccelerationLeft();
   int armPosition = 0;
-
-  Serial.print("Doll arm position X: ");
-  Serial.print(a.acceleration.x);
-  Serial.print(", Y: ");
-  Serial.print(a.acceleration.y);
-  Serial.print(", Z: ");
-  Serial.print(a.acceleration.z);
-
-  if(a.acceleration.x > 9){
-     armPosition = 1;
-     Serial.print("\nArm is Oben!\n");
+  
+  if(rightPos == "seitwärts" && leftPos == "seitwärts") {
+    Serial.print("Standard-Position");
+    armPosition = 0;
   }
-
-  if(a.acceleration.y > 9){
-     armPosition = 2;
-     Serial.print("\nArm irgendwas....!\n");
+  if(rightPos == "oben" || leftPos == "oben") {
+    Serial.print("Freude");
+    armPosition = 1;
   }
-
-  if(a.acceleration.z > 9){
-     armPosition = 3;
-     Serial.print("\nArm is zur Seite!\n");
+  if(rightPos == "gesicht" || leftPos == "gesicht") {
+    Serial.print("Angst");
+    armPosition = 2;
   }
-
-  Serial.println("");
+  if((rightPos == "kopf" && leftPos == "seitwärts") || (rightPos == "seitwärts" && leftPos == "kopf")) {
+    Serial.print("Konzentration");
+    armPosition = 3;
+  }
+  
   return armPosition;
+  Serial.println("");
+  delay(1000);
+}
+
+String getMPUAccelerationRight() {
+  sensors_event_t a, g, temp;
+  mpu_right.getEvent(&a, &g, &temp);
+
+  Serial.print("X-rechts:");
+  Serial.print(a.acceleration.x);
+  Serial.print("Y-rechts:");
+  Serial.print(a.acceleration.y);
+  Serial.print("Z-rechts:");
+  Serial.print(a.acceleration.z);
+  Serial.println("");
+
+  if(a.acceleration.x < 3 && a.acceleration.x > -1) {
+    if(a.acceleration.y < -8 && a.acceleration.y > -11) {
+      if(a.acceleration.z < 1 && a.acceleration.z > -3) {
+        Serial.print("rechts seitwärts");
+        return "seitwärts";
+      }
+    }
+  }
+
+  if(a.acceleration.x < 11 && a.acceleration.x > 7) {
+    if(a.acceleration.y < -1 && a.acceleration.y > -7) {
+      if(a.acceleration.z < 6 && a.acceleration.z > 0) {
+        Serial.print("rechts oben");
+        return "oben";
+      }
+    }
+  }
+
+  if(a.acceleration.x < 8 && a.acceleration.x > 4) {
+    if(a.acceleration.y < -6 && a.acceleration.y > -10) {
+      if(a.acceleration.z < 5 && a.acceleration.z > 1) {
+        Serial.print("rechts gesicht");
+        return "gesicht";
+      }
+    }
+  }
+
+  if(a.acceleration.x < 10 && a.acceleration.x > 6) {
+    if(a.acceleration.y < 1 && a.acceleration.y > -5) {
+      if(a.acceleration.z < 7 && a.acceleration.z > 3) {
+        Serial.print("rechts kopf");
+        return "kopf";
+      }
+    }
+  }
+
+  delay(1000);
+}
+
+String getMPUAccelerationLeft() {
+  sensors_event_t a, g, temp;
+  mpu_left.getEvent(&a, &g, &temp);
+
+  Serial.print("X-links:");
+  Serial.print(a.acceleration.x);
+  Serial.print("Y-links:");
+  Serial.print(a.acceleration.y);
+  Serial.print("Z-links:");
+  Serial.print(a.acceleration.z);
+  Serial.println("");
+
+  if(a.acceleration.x < 2 && a.acceleration.x > -4) {
+    if(a.acceleration.y < 11 && a.acceleration.y > 6) {
+      if(a.acceleration.z < 2 && a.acceleration.z > -5) {
+        Serial.print("links seitwärts");
+        return "seitwärts";
+      }
+    }
+  }
+
+  if(a.acceleration.x < 11 && a.acceleration.x > 7) {
+    if(a.acceleration.y < 5 && a.acceleration.y > 0) {
+      if(a.acceleration.z < 5 && a.acceleration.z > -2) {
+        Serial.print("links oben");
+        return "oben";
+      }
+    }
+  }
+
+  if(a.acceleration.x < 10 && a.acceleration.x > 6) {
+    if(a.acceleration.y < 8 && a.acceleration.y > 4) {
+      if(a.acceleration.z < 5 && a.acceleration.z > -1) {
+        Serial.print("links gesicht");
+        return "gesicht";
+      }
+    }
+  }
+
+  /*if(a.acceleration.x < 11 && a.acceleration.x > 8) {
+    if(a.acceleration.y < 3 && a.acceleration.y > -3) {
+      if(a.acceleration.z < 5 && a.acceleration.z > 1) {
+        Serial.print("links kopf");
+        return "kopf";
+      }
+    }
+  }*/
+
+  delay(1000);
 }
